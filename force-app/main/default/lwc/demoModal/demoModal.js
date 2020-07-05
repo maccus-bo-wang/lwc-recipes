9956ab3860cb5015233a4dc725aee6eb0f39899a
Wang <maccus.wang@cn.imshealth.com>
2020-07-05 15:06:54 +0800

    0705

---
+++ force-app/main/default/lwc/demoModal/demoModal.js
@@ -0,0 +1,18 @@
+import { LightningElement,track } from 'lwc';
+export default class ModalPopupLWC extends LightningElement {
+    //Boolean tracked variable to indicate if modal is open or not default value is false as modal is closed when page is loaded 
+    @track isModalOpen = false;
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
+    }
+}
---
+++ force-app/main/default/lwc/demoSampleApply/demoSampleApply.js
@@ -0,0 +1,98 @@
+import { LightningElement, track, wire } from 'lwc';
+//Datatable Start
+const data = [
+    { id: 1, name: 'Billy Simonns', age: 40, email: 'billy@salesforce.com' },
+    { id: 2, name: 'Kelsey Denesik', age: 35, email: 'kelsey@salesforce.com' },
+    { id: 3, name: 'Kyle Ruecker', age: 50, email: 'kyle@salesforce.com' },
+    {
+        id: 4,
+        name: 'Krystina Kerluke',
+        age: 37,
+        email: 'krystina@salesforce.com',
+    },
+];
+
+const columns = [
+    { label: 'Name', fieldName: 'name' },
+    {
+        label: 'Age',
+        fieldName: 'age',
+        type: 'number',
+        sortable: true,
+        cellAttributes: { alignment: 'left' },
+    },
+    { label: 'Email', fieldName: 'email', type: 'email' },
+];
+//Datatable End
+export default class DemoSampleApply extends LightningElement {
+    value = 'inProgress';
+    @track val1 = undefined;
+    @track val2 = undefined;
+    @track isShow = false;
+
+    get options1() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
+
+    get options2() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
+
+    handleChange(event) {
+        //this.value = event.detail.value;
+        
+        this.val1 = this.val2 = event.detail.value;
+        this.isShow = !this.isShow;
+    }
+
+    handleClick(event) {
+        const buttonLabel= event.target.label;
+        console.log("In handClick!");
+        //  Handle clicks
+        var tab2 = (this.template.querySelectorAll('lightning-tab'))[1];
+        tab2.click();
+        console.log("After handClick:" + tab2.name);
+    }
+
+    //Data table block
+    @track data = data;
+    @track columns = columns;
+    @track defaultSortDirection = 'asc';
+    @track sortDirection = 'asc';
+    @track sortedBy;
+
+    // Used to sort the 'Age' column
+    sortBy(field, reverse, primer) {
+        const key = primer
+            ? function(x) {
+                  return primer(x[field]);
+              }
+            : function(x) {
+                  return x[field];
+              };
+
+        return function(a, b) {
+            a = key(a);
+            b = key(b);
+            return reverse * ((a > b) - (b > a));
+        };
+    }
+
+    onHandleSort(event) {
+        const { fieldName: sortedBy, sortDirection } = event.detail;
+        const cloneData = [...this.data];
+
+        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
+        this.data = cloneData;
+        this.sortDirection = sortDirection;
+        this.sortedBy = sortedBy;
+    }
+}
