0e9f1f6bc5ed7b47ddb6306ef2a3d6a7d1e4e217
Wang <maccus.wang@cn.imshealth.com>
2020-07-09 20:17:51 +0800

    087777

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -3,6 +3,6 @@ echo "Converting to MDAPI format..."
 sfdx force:source:convert -d deploy_prod_test -r force-app 
 #Deploy to prod & run all tests
 echo "Deploying to production & running --testlevel RunSpecifiedTests..."
-#sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l RunAllTestsInOrg
-#sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 --testlevel RunSpecifiedTests HelloWorldTest
-sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l NoTestRun
+#sfdx force:mdapi:deploy -u DevHub -d deploy_prod_test/ -w -1 -l RunAllTestsInOrg 
+#sfdx force:mdapi:deploy -u DevHub -d deploy_prod_test/ -w -1 --testlevel RunSpecifiedTests HelloWorldTest
+sfdx force:mdapi:deploy -u DevHub -d deploy_prod_test/ -w -1 -l NoTestRun
