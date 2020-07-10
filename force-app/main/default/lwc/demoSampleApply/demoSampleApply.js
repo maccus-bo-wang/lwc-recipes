e50a21a72cf6c1c228d4ba70045ce5c6c5e779cd
Wang <maccus.wang@cn.imshealth.com>
2020-07-10 11:58:39 +0800

    0710xxx

--- force-app/main/default/lwc/demoSampleApply/demoSampleApply.js
+++ force-app/main/default/lwc/demoSampleApply/demoSampleApply.js
@@ -95,4 +95,21 @@ export default class DemoSampleApply extends LightningElement {
         this.sortDirection = sortDirection;
         this.sortedBy = sortedBy;
     }
+    
+    //Window
+    //Boolean tracked variable to indicate if modal is open or not default value is false as modal is closed when page is loaded 
+    @track isModalOpen = false;
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
+    }
 }
